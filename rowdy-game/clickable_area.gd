310c787cc37da32c27d079ddfa4061bc624efcd7
Student BE106 <Student.BE106@chaffey.edu>
2025-04-17 16:20:35 -0700

    rework clickable area

--- rowdy-game/clickable_area.gd
+++ rowdy-game/clickable_area.gd
@@ -1,16 +1,20 @@
-extends Area2D
+extends StaticBody2D
 
 var clickable = false
-@onready var colli: CollisionShape2D = $CollisionShape2D
-
+@onready var collider: CollisionShape2D = $CollisionShape2D
+@export var action_script: Script
 
 func  _ready() -> void:
-	if !colli.shape:
+	if !collider.shape:
 		printerr("No Shape given to clickable: ")
 		printerr(self)
 
-func _on_mouse_entered() -> void:
-	clickable = true
-
-func _on_mouse_exited() -> void:
-	clickable = false
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Click"):
+		var space_state = get_world_2d().direct_space_state
+		var point:PhysicsPointQueryParameters2D = PhysicsPointQueryParameters2D.new()
+		point.position = get_global_mouse_position()
+		var result = space_state.intersect_point(point)
+		for dict in result:
+			if dict["collider"] == self:
+				print("Do Stuff Here")

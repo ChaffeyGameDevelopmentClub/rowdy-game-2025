32f67b7dc8cf9f75c3faca5b18d4a8146d1e2ffa
Student BE106 <Student.BE106@chaffey.edu>
2025-04-17 16:27:26 -0700

    basic hub layout?

---
+++ rowdy-game/scenes/hub.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _on_hub_1_pressed() -> void:
+	print("Hub 1 pressed!")
+
+
+func _on_hub_2_pressed() -> void:
+	print("Hub 2 pressed!")
+
+
+func _on_hub_3_pressed() -> void:
+	print("Hub 3 pressed!")
+
+
+func _on_hub_4_pressed() -> void:
+	print("Hub 4 pressed!")

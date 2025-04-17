a8706af4fc3eb2a4ab61ad59d33bf90c2c5766b1
Student BE106 <juniorachi128@hotmail.com>
2025-04-17 16:30:25 -0700

    this is awful

--- rowdy-game/rhythm-game/scenes/notes/hit.gd
+++ rowdy-game/rhythm-game/scenes/notes/hit.gd
@@ -5,5 +5,20 @@ extends Node2D
 
 func _on_hit_area_entered(area: Area2D) -> void:
 	if area.is_in_group("right_beat") and Input.is_action_just_pressed("ui_accept"):
-		area.get_parent().get_node("Right-beat")
+		area.get_parent().hit()
+		print(area.get_parent())
+		print("True!")
 		pass
+
+
+func _on_too_early_area_entered(area: Area2D) -> void:
+	if area.is_in_group("right_beat") and Input.is_action_just_pressed("ui_accept"):
+		area.get_parent().hit()
+		print(area.get_parent())
+		print("Too early")
+		pass
+
+
+func _on_hit_area_exited(area: Area2D) -> void:
+	area.get_parent().hit()
+	print("late")
